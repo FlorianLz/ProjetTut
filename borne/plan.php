322407d0fa0c49fa51b87a2c936bd3a1edbc66cb
FlorianLz <46797434+FlorianLz@users.noreply.github.com>
2019-06-04 08:29:54 +0200

    Modification CSS plan.

--- borne/plan.php
+++ borne/plan.php
@@ -32,91 +32,92 @@
         </div>
         <div class="row">
             <div class="col-xl-8 col-xs-8 col-md-8">
-            <svg id="Carte" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 544.5 800.5">
-                <defs>
-                    <style>.cls-1{fill:#e52828;}.cls-1,.cls-2{stroke:#e52828;}.cls-1,.cls-2,.cls-5{stroke-miterlimit:10;}.cls-2,.cls-5{fill:none;}.cls-3{font-size:19.13px;font-family:Roboto-Medium, Roboto;}.cls-4{letter-spacing:-0.01em;}.cls-5{stroke:#070707;}
-                    </style>
-                    <image id="image" width="50" height="50" xlink:href="icons8-escalator-50.png"/>
-                    <image id="image-2" width="50" height="50" xlink:href="icons8-toilettes-50.png"/>
-                    <image id="image-3" width="50" height="50" xlink:href="icons8-nourriture-50.png"/>
-                    <image id="image-4" width="50" height="50" xlink:href="icons8-montée-d&apos;escaliers-50.png"/>
-                </defs>
-                <g id="Stands">
-                    <rect id="Indispo" class="cls-1" x="1" y="682.5" width="130" height="43"/>
-                    <rect id="Stand_1" class="cls-2" x="12" y="555" width="174.5" height="90.5"/>
-                    <rect id="Stand_2" class="cls-2" x="155" y="652.29" width="63.5" height="64.21"/>
-                    <rect id="Stand_3" class="cls-2" x="260" y="555" width="57" height="40.5"/>
-                    <rect id="Stand_4" class="cls-2" x="327.5" y="555" width="34.5" height="40.5"/>
-                    <rect id="Stand_5" class="cls-2" x="260" y="603.5" width="57" height="57"/>
-                    <rect id="Stand_6" class="cls-2" x="327.5" y="603.5" width="34.5" height="57"/>
-                    <rect id="Stand_7" class="cls-2" x="260" y="668.5" width="102" height="48"/>
-                    <rect id="Stand_8" class="cls-2" x="405" y="555" width="48.5" height="24.5"/>
-                    <rect id="Stand_8-2" data-name="Stand_8" class="cls-2" x="462" y="555" width="71" height="24.5"/>
-                    <rect id="Stand_9" class="cls-2" x="405" y="587" width="48.5" height="71.5"/>
-                    <rect id="Stand_10" class="cls-2" x="461.5" y="587" width="71" height="71"/>
-                    <rect id="Stand_11" class="cls-2" x="405" y="668.5" width="48.5" height="21"/>
-                    <rect id="Stand_12" class="cls-2" x="462" y="664" width="32" height="51.5"/>
-                    <rect id="Stand_13" class="cls-2" x="501" y="664" width="32" height="51.5"/>
-                    <rect id="Stand_14" class="cls-2" x="405" y="695.5" width="48.5" height="21"/>
-                    <rect id="Stand_15" class="cls-2" x="12" y="425.5" width="42" height="92"/>
-                    <rect id="Stand_16" class="cls-2" x="61.5" y="425.5" width="74.5" height="92"/>
-                    <rect id="Stand_17" class="cls-2" x="144.5" y="425.5" width="73" height="92"/>
-                    <rect id="Stand_18" class="cls-2" x="260" y="425" width="102" height="15.5"/>
-                    <rect id="Stand_19" class="cls-2" x="259.5" y="447" width="48" height="49.5"/>
-                    <rect id="Stand_20" class="cls-2" x="313.5" y="447" width="48" height="49.5"/>
-                    <rect id="Stand_21" class="cls-2" x="260" y="502" width="102" height="15.5"/>
-                    <rect id="Stand_22" class="cls-2" x="405" y="425.5" width="76.5" height="92"/>
-                    <rect id="Stand_23" class="cls-2" x="490" y="425.5" width="42" height="92"/>
-                    <polyline id="Stand_24" class="cls-2" points="12 388.5 103.5 388.5 103.5 323.5 12 323.5 12 388.5"/>
-                    <polyline id="Stand_25" class="cls-2" points="147 388.5 111.5 388.5 111.5 323.5 147 323.5 147 388.5"/>
-                    <polyline id="Toilettes" class="cls-2" points="154.5 388 217.5 388 217.5 324 154.5 324 154.5 388"/>
-                    <rect id="Stand_26" class="cls-2" x="260" y="324" width="102" height="20"/>
-                    <rect id="Stand_27" class="cls-2" x="260" y="351" width="102" height="36.5"/>
-                    <polyline id="Toilettes-2" data-name="Toilettes" class="cls-2" points="404.5 388 467.5 388 467.5 324 404.5 324 404.5 388"/>
-                    <polyline id="Stand_28" class="cls-2" points="477.5 388 532.5 388 532.5 324 477.5 324 477.5 388"/>
-                    <polygon id="Stand_29" class="cls-2" points="82 202.5 217 202.5 218 112.5 154 112.5 154 137.5 82 137.5 82 202.5"/>
-                    <polyline id="Stand_30" class="cls-2" points="11.5 287 217.5 287 217.5 218 11.5 218 11.5 287"/>
-                    <rect id="Escalator" class="cls-2" x="190" y="3.5" width="27.5" height="99.5"/>
-                    <rect id="Escalator-2" data-name="Escalator" class="cls-2" x="410" y="3.5" width="27.5" height="99.5"/>
-                    <polyline id="Stand_31" class="cls-2" points="259.5 209 362.5 209 362.5 112 259.5 112 259.5 209"/>
-                    <rect id="Stand_32" class="cls-2" x="259.5" y="218" width="63" height="68.5"/>
-                    <rect id="Stand_33" class="cls-2" x="331.5" y="218" width="31" height="67.5"/>
-                    <rect id="Stand_34" class="cls-2" x="404.5" y="112.5" width="60.5" height="54.5"/>
-                    <rect id="Stand_35" class="cls-2" x="431" y="178" width="102" height="74.5"/>
-                    <rect id="Stand_36" class="cls-2" x="404.5" y="261" width="128.5" height="25.5"/>
-                    <polyline id="Stand_37" class="cls-2" points="233.5 98 388.5 98 388.5 4 233.5 4 233.5 98"/>
-                    <use id="Logo_escalator" transform="translate(193.5 41) scale(0.42)" xlink:href="#image"/>
-                    <use id="Logo_escalator-2" data-name="Logo_escalator" transform="translate(439 67) scale(0.42)" xlink:href="#image"/>
-                    <use id="Logo_escalator-3" data-name="Logo_escalator" transform="translate(264 800) scale(0.42)" xlink:href="#image"/>
-                    <use id="Logo_escalator-4" data-name="Logo_escalator" transform="translate(413 800) scale(0.42)" xlink:href="#image"/>
-                    <image id="Logo_infirmerie" width="50" height="50" transform="translate(479.5 605) scale(0.7)" xlink:href="icons8-hôpital-50.png"/>
-                    <image id="Logo_information" width="50" height="50" transform="translate(465.5 676) scale(0.54)" xlink:href="icons8-information-50.png"/>
-                    <use id="Logo_toilettes" transform="translate(181.5 432) scale(0.48)" xlink:href="#image-2"/>
-                    <use id="Logo_escalator-5" data-name="Logo_escalator" transform="translate(180 492) scale(0.42)" xlink:href="#image"/>
-                    <use id="Logo_escalator-6" data-name="Logo_escalator" transform="translate(436 490) scale(0.42)" xlink:href="#image"/>
-                    <use id="Logo_toilettes-2" data-name="Logo_toilettes" transform="translate(475 458) scale(0.48)" xlink:href="#image-2"/>
-                    <use id="Logo_restauration" transform="translate(295.5 30) scale(0.72)" xlink:href="#image-3"/>
-                    <use id="Logo_restauration-2" data-name="Logo_restauration" transform="translate(476 511) scale(0.46)" xlink:href="#image-3"/>
-                    <image id="Logo_flèche" width="50" height="50" transform="translate(223.5 716) scale(0.6)" xlink:href="icons8-flèche-haut-50.png"/>
-                    <use id="Logo_escaliers" transform="translate(218.5 773) scale(0.4)" xlink:href="#image-4"/>
-                    <use id="Logo_escaliers-2" data-name="Logo_escaliers" transform="translate(393 799) scale(0.4)" xlink:href="#image-4"/>
-                    <text class="cls-3" transform="translate(283.5 738.84)">Ent<tspan class="cls-4" x="27.98" y="0">r</tspan><tspan x="34.51" y="0">ée</tspan></text>
-                </g>
-                <polygon id="Contour" class="cls-5" points="544 167.5 472.01 167.5 472.5 101 440.71 101 440 99.5 440 0.5 187.5 1 187.5 100 140.5 100 140.5 125.5 68 125.5 68 210.5 0.5 211 0.5 727 179.5 727 179.5 800 491.5 800 491 726.5 544 726.5 544 167.5"/>
+                <svg id="Carte" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 544.5 800.5">
+                    <defs>
+                        <style>.cls-1{fill:#e52828;}.cls-1,.cls-2{stroke:#e52828;}.cls-1,.cls-2,.cls-5{stroke-miterlimit:10;}.cls-2,.cls-5{fill:none;}.cls-3{font-size:19.13px;font-family:Roboto-Medium, Roboto;}.cls-4{letter-spacing:-0.01em;}.cls-5{stroke:#070707;}
+                        </style>
+                        <image id="image" width="50" height="50" xlink:href="icons8-escalator-50.png"/>
+                        <image id="image-2" width="50" height="50" xlink:href="icons8-toilettes-50.png"/>
+                        <image id="image-3" width="50" height="50" xlink:href="icons8-nourriture-50.png"/>
+                        <image id="image-4" width="50" height="50" xlink:href="icons8-montée-d&apos;escaliers-50.png"/>
+                    </defs>
+                    <g id="Stands">
+                        <rect id="Indispo" class="cls-1" x="1" y="682.5" width="130" height="43"/>
+                        <rect id="Stand_1" class="cls-2" x="12" y="555" width="174.5" height="90.5"/>
+                        <rect id="Stand_2" class="cls-2" x="155" y="652.29" width="63.5" height="64.21"/>
+                        <rect id="Stand_3" class="cls-2" x="260" y="555" width="57" height="40.5"/>
+                        <rect id="Stand_4" class="cls-2" x="327.5" y="555" width="34.5" height="40.5"/>
+                        <rect id="Stand_5" class="cls-2" x="260" y="603.5" width="57" height="57"/>
+                        <rect id="Stand_6" class="cls-2" x="327.5" y="603.5" width="34.5" height="57"/>
+                        <rect id="Stand_7" class="cls-2" x="260" y="668.5" width="102" height="48"/>
+                        <rect id="Stand_8" class="cls-2" x="405" y="555" width="48.5" height="24.5"/>
+                        <rect id="Stand_8-2" data-name="Stand_8" class="cls-2" x="462" y="555" width="71" height="24.5"/>
+                        <rect id="Stand_9" class="cls-2" x="405" y="587" width="48.5" height="71.5"/>
+                        <rect id="Stand_10" class="cls-2" x="461.5" y="587" width="71" height="71"/>
+                        <rect id="Stand_11" class="cls-2" x="405" y="668.5" width="48.5" height="21"/>
+                        <rect id="Stand_12" class="cls-2" x="462" y="664" width="32" height="51.5"/>
+                        <rect id="Stand_13" class="cls-2" x="501" y="664" width="32" height="51.5"/>
+                        <rect id="Stand_14" class="cls-2" x="405" y="695.5" width="48.5" height="21"/>
+                        <rect id="Stand_15" class="cls-2" x="12" y="425.5" width="42" height="92"/>
+                        <rect id="Stand_16" class="cls-2" x="61.5" y="425.5" width="74.5" height="92"/>
+                        <rect id="Stand_17" class="cls-2" x="144.5" y="425.5" width="73" height="92"/>
+                        <rect id="Stand_18" class="cls-2" x="260" y="425" width="102" height="15.5"/>
+                        <rect id="Stand_19" class="cls-2" x="259.5" y="447" width="48" height="49.5"/>
+                        <rect id="Stand_20" class="cls-2" x="313.5" y="447" width="48" height="49.5"/>
+                        <rect id="Stand_21" class="cls-2" x="260" y="502" width="102" height="15.5"/>
+                        <rect id="Stand_22" class="cls-2" x="405" y="425.5" width="76.5" height="92"/>
+                        <rect id="Stand_23" class="cls-2" x="490" y="425.5" width="42" height="92"/>
+                        <polyline id="Stand_24" class="cls-2" points="12 388.5 103.5 388.5 103.5 323.5 12 323.5 12 388.5"/>
+                        <polyline id="Stand_25" class="cls-2" points="147 388.5 111.5 388.5 111.5 323.5 147 323.5 147 388.5"/>
+                        <polyline id="Toilettes" class="cls-2" points="154.5 388 217.5 388 217.5 324 154.5 324 154.5 388"/>
+                        <rect id="Stand_26" class="cls-2" x="260" y="324" width="102" height="20"/>
+                        <rect id="Stand_27" class="cls-2" x="260" y="351" width="102" height="36.5"/>
+                        <polyline id="Toilettes-2" data-name="Toilettes" class="cls-2" points="404.5 388 467.5 388 467.5 324 404.5 324 404.5 388"/>
+                        <polyline id="Stand_28" class="cls-2" points="477.5 388 532.5 388 532.5 324 477.5 324 477.5 388"/>
+                        <polygon id="Stand_29" class="cls-2" points="82 202.5 217 202.5 218 112.5 154 112.5 154 137.5 82 137.5 82 202.5"/>
+                        <polyline id="Stand_30" class="cls-2" points="11.5 287 217.5 287 217.5 218 11.5 218 11.5 287"/>
+                        <rect id="Escalator" class="cls-2" x="190" y="3.5" width="27.5" height="99.5"/>
+                        <rect id="Escalator-2" data-name="Escalator" class="cls-2" x="410" y="3.5" width="27.5" height="99.5"/>
+                        <polyline id="Stand_31" class="cls-2" points="259.5 209 362.5 209 362.5 112 259.5 112 259.5 209"/>
+                        <rect id="Stand_32" class="cls-2" x="259.5" y="218" width="63" height="68.5"/>
+                        <rect id="Stand_33" class="cls-2" x="331.5" y="218" width="31" height="67.5"/>
+                        <rect id="Stand_34" class="cls-2" x="404.5" y="112.5" width="60.5" height="54.5"/>
+                        <rect id="Stand_35" class="cls-2" x="431" y="178" width="102" height="74.5"/>
+                        <rect id="Stand_36" class="cls-2" x="404.5" y="261" width="128.5" height="25.5"/>
+                        <polyline id="Stand_37" class="cls-2" points="233.5 98 388.5 98 388.5 4 233.5 4 233.5 98"/>
+                        <use id="Logo_escalator" transform="translate(193.5 41) scale(0.42)" xlink:href="#image"/>
+                        <use id="Logo_escalator-2" data-name="Logo_escalator" transform="translate(439 67) scale(0.42)" xlink:href="#image"/>
+                        <use id="Logo_escalator-3" data-name="Logo_escalator" transform="translate(264 800) scale(0.42)" xlink:href="#image"/>
+                        <use id="Logo_escalator-4" data-name="Logo_escalator" transform="translate(413 800) scale(0.42)" xlink:href="#image"/>
+                        <image id="Logo_infirmerie" width="50" height="50" transform="translate(479.5 605) scale(0.7)" xlink:href="icons8-hôpital-50.png"/>
+                        <image id="Logo_information" width="50" height="50" transform="translate(465.5 676) scale(0.54)" xlink:href="icons8-information-50.png"/>
+                        <use id="Logo_toilettes" transform="translate(181.5 432) scale(0.48)" xlink:href="#image-2"/>
+                        <use id="Logo_escalator-5" data-name="Logo_escalator" transform="translate(180 492) scale(0.42)" xlink:href="#image"/>
+                        <use id="Logo_escalator-6" data-name="Logo_escalator" transform="translate(436 490) scale(0.42)" xlink:href="#image"/>
+                        <use id="Logo_toilettes-2" data-name="Logo_toilettes" transform="translate(475 458) scale(0.48)" xlink:href="#image-2"/>
+                        <use id="Logo_restauration" transform="translate(295.5 30) scale(0.72)" xlink:href="#image-3"/>
+                        <use id="Logo_restauration-2" data-name="Logo_restauration" transform="translate(476 511) scale(0.46)" xlink:href="#image-3"/>
+                        <image id="Logo_flèche" width="50" height="50" transform="translate(223.5 716) scale(0.6)" xlink:href="icons8-flèche-haut-50.png"/>
+                        <use id="Logo_escaliers" transform="translate(218.5 773) scale(0.4)" xlink:href="#image-4"/>
+                        <use id="Logo_escaliers-2" data-name="Logo_escaliers" transform="translate(393 799) scale(0.4)" xlink:href="#image-4"/>
+                        <text class="cls-3" transform="translate(283.5 738.84)">Ent<tspan class="cls-4" x="27.98" y="0">r</tspan><tspan x="34.51" y="0">ée</tspan></text>
+                    </g>
+                    <polygon id="Contour" class="cls-5" points="544 167.5 472.01 167.5 472.5 101 440.71 101 440 99.5 440 0.5 187.5 1 187.5 100 140.5 100 140.5 125.5 68 125.5 68 210.5 0.5 211 0.5 727 179.5 727 179.5 800 491.5 800 491 726.5 544 726.5 544 167.5"/>
                 </svg>
             </div>
             
             <div class="col-xl-4 col-xs-4 col-md-4 conteneurcartes">
                 <div class="gallery col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 </div>
-                <div class="carte">
-                    <center><div style="width:100%;">
+                <center><div style="width:100%;">
                         <button class="selection btn-default filter-button" data-filter="tout">Tout</button>
                         <button class="selection btn-default filter-button" data-filter="vente">Vente</button>
                         <button class="selection btn-default filter-button" data-filter="console">Console</button>
                         <button class="selection btn-default filter-button" data-filter="services">Services</button>
                         <button class="selection btn-default filter-button" data-filter="partenaire">Partenaire</button>
-                        </div></center>    
+                        </div></center>
+                <div class="carte">
+                        
                     <?php
                     $exposants = file_get_contents('exposants.json');
                     $exposants = json_decode($exposants, true); 
