f2813a88c65fab841250958dda26327971c54b54
FlorianLz <46797434+FlorianLz@users.noreply.github.com>
2019-06-05 10:19:37 +0200

    Modification style texte de recherche.

--- borne/plan.php
+++ borne/plan.php
@@ -432,7 +432,7 @@ if(isset($_POST['texte']) && $_POST['texte']==''){
                     <?php 
                         if(!isset($_POST['texte']) || $_POST['texte']=='' ){
     
-                            echo'<h3 style="color:white;">Aucune recherche</h3>';
+                            echo'<h3 class="msgrecherche">Aucune recherche</h3>';
                             $exposants = file_get_contents('exposants.json');
                             $exposants = json_decode($exposants, true); 
                             for($i=0;$i<count($exposants['exposants']);$i++){
@@ -446,7 +446,7 @@ if(isset($_POST['texte']) && $_POST['texte']==''){
     
                           }else{
                              $motrecherche=$_POST['texte'];
-                             echo '<h3 style="color:white;">Résultats pour : '.$motrecherche . '</h3>';
+                             echo '<h3 class="msgrecherche">Résultats pour : '.$motrecherche . '</h3>';
                              $motrecherche=strtolower($_POST['texte']);
                              $exposants = file_get_contents('exposants.json');
                              $exposants = json_decode($exposants, true);
