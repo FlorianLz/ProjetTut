818a46587337d5b181c0f3f9eda22a63f414a01f
FlorianLz <46797434+FlorianLz@users.noreply.github.com>
2019-06-09 23:22:43 +0200

    Résolution problème collapse.

--- borne/plan.php
+++ borne/plan.php
@@ -21,10 +21,7 @@ if(isset($_POST['texte']) && $_POST['texte']==''){
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="js/jquery-3.4.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/script.js"></script>
-    
+
   </head>
     
   <body>
@@ -491,6 +488,7 @@ if(isset($_POST['texte']) && $_POST['texte']==''){
     
       
     <!-- Javascript optionnel -->
+      <script src="js/jquery-3.4.1.min.js"></script>
     <script src="js/script.js"></script>
     <script src="js/heure.js"></script>
 
