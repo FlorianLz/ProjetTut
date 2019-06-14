e837b54bdd9a739507854bb357d65930f2fcaa0a
LydieV <49896819+LydieV@users.noreply.github.com>
2019-06-14 19:40:49 +0200

    Création billetterie.

--- js/script.js
+++ js/script.js
@@ -113,3 +113,100 @@ function resetimage(id){
         document.getElementById(id).style.width="30vw";
     }
 }
+
+/* fonctions pour la billetterie */
+
+var total =0;
+var totalD = 0;
+var totalPD = 0;
+var totalS = 0;
+var totalPS = 0;
+var totalDS = 0;
+var totalPDS = 0;
+
+var samplein = 0;
+var prixsamplein = 0;
+
+var samreduit = 0;
+var prixsamreduit = 0;
+
+var samgrat = 0;
+
+var dimplein = 0;
+var prixdimplein = 0;
+
+var dimreduit = 0;
+var prixdimreduit = 0;
+
+var dimgrat = 0;
+
+var dsp =0;
+var prixdsp = 0;
+
+var dsr=0;
+var prixdsr = 0;
+
+var dsg=0;
+
+function calculDtarif(id,a){
+    if(id == "dimgrat"){
+        dimgrat = a*1;
+    }
+    if(id == "dimred"){
+        dimreduit = a*1;
+        prixdimreduit = a*10;
+    }
+    if(id == "dimplein"){
+        dimplein = a*1;
+        prixdimplein = a*15;
+    }
+}
+
+function calculD(){
+    totalD = dimplein +dimreduit + dimgrat;
+    document.getElementById('nbBilletsDim').innerHTML = totalD;
+    totalPD = prixdimplein + prixdimreduit;
+    document.getElementById('prixBilletsDim').innerHTML = totalPD;
+}
+
+function calculStarif(id,a){
+    if(id == "samgrat"){
+        samgrat = a*1;
+    }
+    if(id == "samred"){
+        samreduit = a*1;
+        prixsamreduit = a*10;
+    }
+    if(id == "samplein"){
+        samplein = a*1;
+        prixsamplein = a*15;
+    }
+}
+
+function calculS(){
+    totalS = samplein +samreduit + samgrat;
+    document.getElementById('nbBilletsSam').innerHTML = totalS;
+    totalPS = prixsamplein + prixsamreduit;
+    document.getElementById('prixBilletsSam').innerHTML = totalPS;
+}
+
+function calculSDtarif(id,a){
+    if(id == "dsgrat"){
+        dsg = a*1
+    }
+    if(id == "dsred"){
+        dsr = a *1;
+        prixdsr = a * 17;
+    }
+    if(id == "dsplein"){
+        dsp = a*1;
+        prixdsp = a*25;
+    }
+}
+
+function calculDS(){
+    totalDS = dsg + dsr + dsp;
+    document.getElementById('nbBilletsDimSam').innerHTML = totalDS;
+    totalPDS = prixdsr + prixdsp;
+    document.getElementById('prixBilletsDimSam').innerHTML = totalPDS;
+}
